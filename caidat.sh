691f29a2db928e1e4ffaefb7db8b8c1232f9c4d4
ngocnguyenhong <nhngoc94@gmail.com>
2020-10-21 11:05:45 +0700

    Update caidat.sh

--- caidat.sh
+++ caidat.sh
@@ -4,6 +4,13 @@ tar -xvzf winemesa.tgz
 sudo cp -r winemesa/wine /home/$USER/wine
 sudo cp -r winemesa/gl4es /home/$USER/gl4es
 sudo cp -r winemesa/mesa /home/$USER/mesa
+sudo cp /home/$USER/wine/bin/wine /usr/local/bin
+sudo ln -s /home/$USER/wine/bin/wineserver wineserver
+sudo chmod a+x /usr/local/bin/wine
+sudo chmod a+x /home/$USER/wine/bin/wine
+sudo chmod a+x /home/$USER/wine/bin/wine-preloader
+sudo chmod a+x /home/$USER/wine/bin/wineboot
+sudo chmod a+x /home/$USER/wine/bin/winepath
 sudo rm -rf winemesa
 sudo rm -f winemesa.tgz
 sudo apt -y update
